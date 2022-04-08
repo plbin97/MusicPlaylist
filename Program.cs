b24897103e192bc6de0421e00dd7c02ed5fdc3b1
Linbin <i@teenet.me>
2022-04-08 11:45:07 -0400

    apply okta

--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Okta.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    })
+    .AddCookie(options =>
+    {
+        options.LoginPath = new PathString("/Account/SignIn");
+    })
+    .AddOktaMvc(new OktaMvcOptions
+    {
+        // Replace these values with your Okta configuration
+        OktaDomain = Configuration.GetValue<string>("Okta:OktaDomain"),
+        ClientId = Configuration.GetValue<string>("Okta:ClientId"),
+        ClientSecret = Configuration.GetValue<string>("Okta:ClientSecret"),
+        AuthorizationServerId = Configuration.GetValue<string>("Okta:AuthorizationServerId"),
+        Scope = new List<string> { "openid", "profile", "email" },
+    });
 
 var app = builder.Build();
 
@@ -18,6 +41,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseAuthentication();
 
 app.MapControllerRoute(
     name: "default",
